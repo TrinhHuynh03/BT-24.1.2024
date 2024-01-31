2f63c7f49f29dbab0ccc987527a3f1dacc4ebfc2
TrinhHuynh03 <trinhhuynh.25092003@gmail.com>
2024-01-31 12:51:38 +0700

    rb

---
+++ BT2/main.js
@@ -0,0 +1,7 @@
+for( i=0; i<10;i++)
+{
+    for(j =0;j<5;j++){
+        document.write( `<div class="id"></div>`)
+    }
+    document.write( `<div style='clear:both'></div>`)
+}
---
+++ BT4/main.js
@@ -0,0 +1,11 @@
+var SinhVien = {
+    name:"Huỳnh Ngọc Phương Trinh",
+    year:"2003",
+    gender:"Nữ",
+    hometown:"Quảng Nam"
+}
+
+fro(x in SinhVien) {
+    document.write(SinhVien[x]);
+    document.write("<hr>");
+}
